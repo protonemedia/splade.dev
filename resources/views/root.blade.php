37e305b1f2d6a3a7d9ef3f8559860fb077c3ee42
Pascal Baljet <pascal@pascalbaljet.nl>
2023-01-24 21:28:28 +0100

    Favicons

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -8,6 +8,13 @@
         @googlefonts('display')
 
         @spladeHead
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#b940a2">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="theme-color" content="#ffffff">
         @vite('resources/js/app.js')
 
         @production
